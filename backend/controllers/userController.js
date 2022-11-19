eac7d9a151e47c2e9e4883502192f45a7dbf618f
raktimkakati01 <raktimkakati01@gmail.com>
2022-11-19 17:15:23 +0530

    getUser data

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -158,9 +158,21 @@ const getUser = asyncHandler (async (req, res) => {
     }
 });
 
-//Get login details
+//Get login status
 const loginStatus = asyncHandler (async (req, res) => {
-    res.send("Login Status")
+
+    const token = req.cookies.token;
+    if(!token) {
+        return res.json(false);
+    }
+
+    // verify Token
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    if (verified) {
+        return res.json(true);
+    }
+    return res.json(false);
+
 });
 
 module.exports = {
